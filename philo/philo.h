15c999adf1ee2af5ab14a0e2c3d3b671145a7bcc
ebarbash <ebarbash@student.codam.nl>
2025-08-20 02:08:36 +0200

    Makefile

---
+++ philo/philo.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sergei_pilman <sergei_pilman@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/18 21:46:18 by sergei_pilm       #+#    #+#             */
+/*   Updated: 2025/08/18 22:25:20 by sergei_pilm      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include <sys/time.h>
+#include <unistd.h>
+
